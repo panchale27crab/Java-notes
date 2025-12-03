4ec28a87f2e2ce865947f25c372bd16be7c6f96d
panchale27crab <peric6084@gmail.com>
2025-12-03 18:08:55 +0530

    Add files via upload

---
+++ Javanew/src/javanew/Javanew.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javanew;
+
+//class Home{
+//
+//    void MasterBedroom(){
+//       System.out.println("welcome to the MasterBedroom!");
+//    }
+//
+//    void Room(){
+//        System.out.println("This is my room!");
+//    }
+//    
+//    void Room1(){
+//        System.out.println("This is the guest room1");
+//   }
+//    
+//    void Gate(){
+//        System.out.println("This is the main gate!");
+//    }
+//    
+//    void WashRoom(){
+//        System.out.println("Welcome to my washroom");
+//    }
+//    
+//    void Kitchen(){
+//        System.out.println("This is my sweet Kitchen");
+//    }
+//    
+//    static void WaterTank(){
+//        System.out.println("This is our watertank");
+//    }
+//    static void Bridge(){
+//        System.out.println("The bridge that leads you home.");
+//        }
+//}
+//
+//class koththu{
+//    koththu(){
+//     System.out.println("Hi I am the constructor!");
+//    }
+//   
+//    koththu(String Type, String Size, String Note){
+//     System.out.println( "Hi I am customised constructor");
+//    }
+//    koththu(String price){}
+//    
+//    public void generateKoththu_1(){
+//    System.out.println("Hi my bro I am default constructor");
+//    }
+//    public void generateKoththu_2(){
+//    System.out.println("Hi I am customised constructor!");
+//    }
+//}
+//public class Javanew {
+//   public static void main(String[] args) {
+//       
+//       koththu koththu1 = new koththu();
+//       koththu koththu2 = new koththu("Cheese", "Full", "Extra Cheese"); 
+//       koththu koththu3 = new koththu("three thousand");
+//   }
+//}
+//
+//
+//   int x=500;
+//    Home panchale = new Home();
+//    panchale.Gate();
+//    panchale.WashRoom();
+//    
+//    Home kevin = new Home();
+//    kevin.Kitchen();
+//    kevin.Room1();
+//   
+//    Home.WaterTank();
+//    Home.Bridge();
+//   
+
+   
+    
+
